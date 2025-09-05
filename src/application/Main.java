c1dd2780b0cb7ec4a2ffd40a2877bf69d68e6a33
RonaldoMacedo <ronni29_100@hotmail.com>
2025-09-04 22:20:57 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,15 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
+	//*************************************************************************************************************************************************************
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,7 +28,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Gestão de estoque");
 			primaryStage.show();
