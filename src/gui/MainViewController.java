8daabe67b0c6bb2ad79b90526b45f421e791cec0
RonaldoMacedo <ronni29_100@hotmail.com>
2025-09-06 21:08:56 -0300

    ProductList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -31,7 +31,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemListarProdutosAction() {
-		System.out.println("Lista de produtos");
+		loadView("/gui/ProductList.fxml");
 	}
 	
 	//*************************************************************************************************************************************************************
