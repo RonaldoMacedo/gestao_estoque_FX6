74f881fbf2588c38d7ad514016864af085538ebb
RonaldoMacedo <ronni29_100@hotmail.com>
2025-09-11 10:46:18 -0300

    ProductForm controller

---
+++ src/gui/ProductFormController.java
@@ -0,0 +1,66 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ProductFormController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldMaxLength(txtDescricaoInterna, 200);
+	}
+	
+	//*************************************************************************************************************************************************************
+	
+	@FXML
+	private TextField txtCodigo;
+	
+	@FXML
+	private TextField txtDescricaoInterna;
+	
+	@FXML
+	private TextField txtDataCadastro;
+	
+	@FXML
+	private TextField txtGrupo;
+	
+	@FXML
+	private TextField txtSituacao;
+	
+	//*************************************************************************************************************************************************************
+	
+	@FXML
+	private Label lblErro;
+	
+	//*************************************************************************************************************************************************************
+	
+	@FXML
+	private Button btSalvar;
+	
+	@FXML
+	public void onBtSalvarAction() {
+		System.out.println("Salvar");
+	}
+	
+	@FXML
+	private Button onBtCancelar;
+	
+	@FXML
+	public void onBtCancelarAction() {
+		System.out.println("Cancelar");
+	}
+	
+	//*************************************************************************************************************************************************************
+
+}
