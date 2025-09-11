ed52b584f76e582c9424f471fd578f2adbb2402d
RonaldoMacedo <ronni29_100@hotmail.com>
2025-09-11 09:55:42 -0300

    ProductForm dialog design

--- src/gui/ProductListController.java
+++ src/gui/ProductListController.java
@@ -1,19 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Product;
 import model.services.ProductService;
@@ -26,7 +35,7 @@ public class ProductListController implements Initializable {
 		this.service = service;
 	}
 	
-	//*************************************************************************************************************************************************************
+	//************************************************************************************************************************************************************
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -46,22 +55,24 @@ public class ProductListController implements Initializable {
 		tableViewProduct.prefHeightProperty().bind(stage.heightProperty());
 	}
 	
-	//*************************************************************************************************************************************************************
+	//************************************************************************************************************************************************************
 
 	@FXML
 	private Button btNovo;
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("Formulário de novo produto");
+	public void onBtNovoAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/ProductForm.fxml", parentStage);
+		
 	}
 	
-	//*************************************************************************************************************************************************************
+	//************************************************************************************************************************************************************
 	
 	@FXML
 	private Button btAtualizar;
 	
-	//*************************************************************************************************************************************************************
+	//************************************************************************************************************************************************************
 	
 	@FXML
 	private TableView<Product> tableViewProduct;
@@ -84,7 +95,7 @@ public class ProductListController implements Initializable {
 	@FXML
 	private TableColumn<Product, Integer> tableColumnSaldo;
 	
-	//*************************************************************************************************************************************************************
+	//************************************************************************************************************************************************************
 	
 	private ObservableList<Product> obsList;
 	
@@ -95,6 +106,25 @@ public class ProductListController implements Initializable {
 		List<Product> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewProduct.setItems(obsList);
+	}
+	
+	//************************************************************************************************************************************************************
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Digite os dados do produto");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}catch(IOException e) {
+			Alerts.showAlerts("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
